f81bc7f2e1074519784819a5f2c68740fda82180
Helton Carlos de Souza <heltoncarlossouza@gmail.com>
2017-07-16 18:57:53 -0300

    Create linear data flow with container style types (Box) [Adding fold]

--- functional/composable-functional-javascript/01-linear-data-flow.js
+++ functional/composable-functional-javascript/01-linear-data-flow.js
@@ -1,13 +1,16 @@
 const Box = x => ({
-  map: f => Box(f(x))
+  map: f => Box(f(x)),
+  fold: f => f(x),
+  inspect: () => `Box(${x})`
 })
 
 const nextCharForNumberString = str =>
   Box(str)
   .map(s => s.trim())
-  .map(s => parseInt(s))
+  .map(s => new Number(s))
   .map(i => i + 1)
   .map(i => String.fromCharCode(i))
+  .fold(c => c.toLowerCase())
 
 const result = nextCharForNumberString(' 64 ')
 
