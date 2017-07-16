d9e9bf9d22b74362ede4dc537ca6d45a097c03d2
Helton Carlos de Souza <heltoncarlossouza@gmail.com>
2017-07-16 19:40:50 -0300

    Enforce a null check with composable code branching using Either [Second example]

--- functional/composable-functional-javascript/03-enforce-null-check-with-Either.js
+++ functional/composable-functional-javascript/03-enforce-null-check-with-Either.js
@@ -12,9 +12,42 @@ const Left = x => ({
   inspect: () => `Left(${x})`
 })
 
-const result =
+const fromNullable = value =>
+  value != null ? Right(value) : Left(null)
+
+//-------------------------------------
+
+console.log(
   Right(3)
   .map(x =>  x + 1)
-  .fold(x => "error", x => x)
+  .fold(x => "error",
+        x => x)
+)
 
-console.log(result)
+console.log(
+  Left(3)
+  .map(x =>  x + 1)
+  .fold(x => "error",
+        x => x)
+)
+
+//-------------------------------------
+
+const findColor = name =>
+  fromNullable({ red: '#ff4444', blue: '#3b5998', yellow: '#fff68f' }[name])
+
+console.log(
+  findColor('red')
+  .map(s => s.slice(1))
+  .map(s => s.toUpperCase())
+  .fold(x => "error",
+        x => x)
+)
+
+console.log(
+  findColor('green')
+  .map(s => s.slice(1))
+  .map(s => s.toUpperCase())
+  .fold(x => "error",
+        x => x)
+)
