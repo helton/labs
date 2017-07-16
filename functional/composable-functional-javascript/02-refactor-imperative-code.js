98581cf48230be84ee18f6d2c3fcf512829ac24d
Helton Carlos de Souza <heltoncarlossouza@gmail.com>
2017-07-16 19:17:06 -0300

    Refactor imperative code to a single composed expression using Box [Refactored to use Box]

--- functional/composable-functional-javascript/02-refactor-imperative-code.js
+++ functional/composable-functional-javascript/02-refactor-imperative-code.js
@@ -5,16 +5,23 @@ const Box = x => ({
 })
 
 const moneyToFloat = str =>
-  parseFloat(str.replace(/\$/g, ''))
+  Box(str)
+  .map(s => s.replace(/\$/g, ''))
+  .map(s => parseFloat(s))
 
-const percentToFloat = str => {
-  const replaced = str.replace(/\%/g, '')
-  const number = parseFloat(replaced)
-  return number * 0.01
-}
+const percentToFloat = str =>
+  Box(str.replace(/\%/g, ''))
+  .map(replaced => parseFloat(replaced))
+  .map(number => number * 0.01)
 
-const applyDiscount = (price, discount) => {
-  const cost = moneyToFloat(price)
-  const savings = percentToFloat(discount)
-  return cost - savings
-}
+const applyDiscount = (price, discount) =>
+  moneyToFloat(price)
+  .fold(cost =>
+    percentToFloat(discount)
+    .fold(savings =>
+      cost - cost * savings
+    )
+  )
+
+const result = applyDiscount('$5.00', '20%')
+console.log(result)
