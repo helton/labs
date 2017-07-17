f26b8f6e2e6eb9dad50fb5de7a94bbb15eea6716
Helton Carlos de Souza <heltoncarlossouza@gmail.com>
2017-07-17 02:01:10 -0300

    Create types with Semigroups

---
+++ functional/composable-functional-javascript/06-semigroups.js
@@ -0,0 +1,43 @@
+const { log, logx } = require('./utils.js')
+
+logx('Semigroup String',
+  'a'.concat('b').concat('c')
+)
+
+logx('Semigroup Array (using associativity law)',
+  [1, 2].concat([3, 4].concat([5, 6]))
+)
+
+const Sum = x => ({
+  x,
+  concat: ({x : y}) => Sum(x + y),
+  inspect: () => `Sum(${x})`
+})
+
+logx("Sum Semigroup",
+  Sum(1).concat(Sum(2))
+)
+
+const All = x => ({
+  x,
+  concat: ({x : y}) => All(x && y),
+  inspect: () => `All(${x})`
+})
+
+logx("All Semigroup - example 2",
+  All(true).concat(All(false))
+)
+
+logx("All Semigroup - example 2",
+  All(true).concat(All(true))
+)
+
+const First = x => ({
+  x,
+  concat: _ => First(x)
+  inspect: () => `First(${x})`
+})
+
+logx("First Semigroup",
+  First('blah').concat('foo').concat('bar')
+)
--- functional/composable-functional-javascript/utils.js
+++ functional/composable-functional-javascript/utils.js
@@ -1,4 +1,4 @@
 const log = content => console.log(content)
-const logx = (message, content) => log(`[${message}]\n=> ${content}`)
+const logx = (message, content) => console.log(`[${message}]\n=> `, content)
 
 module.exports = { log, logx }
