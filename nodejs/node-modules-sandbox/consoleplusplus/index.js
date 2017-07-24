b8e90876d96d869c79276290ee84a8ff922a56dd
Helton Carlos de Souza <heltoncarlossouza@gmail.com>
2017-07-23 21:26:27 -0300

    Testing consoleplusplus module

---
+++ nodejs/node-modules-sandbox/consoleplusplus/index.js
@@ -0,0 +1,11 @@
+require('consoleplusplus')
+
+console.log("A (boring) console.log message");
+
+console.debug("An (irrelevant) console.debug message");
+
+console.info("A (somewhat important) console.info message");
+
+console.warn("A (quite important) console.warning message");
+
+console.error("A (critical!) console.error message");
