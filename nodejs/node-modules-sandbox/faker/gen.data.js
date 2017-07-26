66427493edca642f33d7bcb3392ebec737c4e96d
Helton Carlos de Souza <heltoncarlossouza@gmail.com>
2017-07-26 20:06:39 -0300

    Refactoring faker testing project

--- nodejs/node-modules-sandbox/faker/gen.data.js
+++
@@ -1,6 +0,0 @@
-const faker = require('faker')
-
-console.log(faker.name.firstName(), faker.name.lastName())
-console.log(faker.helpers.createCard())
-console.log(faker.internet.avatar())
-console.log(faker.internet.email())
